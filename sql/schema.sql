e606eb47e8c5a24b136ef801ba87d7dbe7097b11
Tamir <tmelzerb@gmail.com>
2023-03-02 10:31:14 -0800

    Added user table and content to databases.sql

---
+++ sql/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+  id UUID default uuid_generate_v4() UNIQUE PRIMARY KEY, 
+  username VARCHAR(64) UNIQUE NOT NULL, 
+  email VARCHAR(64) UNIQUE NOT NULL, 
+  password VARCHAR(64) NOT NULL, 
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(), 
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
